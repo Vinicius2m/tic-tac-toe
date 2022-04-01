f4ce4ecb35d2642237f8e1b806dcbcd83f322e66
Vinícius de Freitas <viniciusfreitas.1.4.4@gmail.com>
2022-04-01 04:26:00 -0300

    adding win verification logic and styling the page

--- script.js
+++ script.js
@@ -1,71 +1,115 @@
-const mainGame = document.querySelector(".mainGame")
-const startButton = document.getElementById("startButton")
-const gameContainer = document.querySelector(".gameContainer")
+const mainGame = document.querySelector('.mainGame');
+const startButton = document.getElementById('startButton');
+const gameContainer = document.querySelector('.gameContainer');
 
-let player = true
+let player = true;
 
-const table = [
-    "SSS",
-    "SSS",
-    "SSS"
-]
+let playerXSelections = [];
+let playerOSelections = [];
 
-startButton.addEventListener("click", startGame)
+const winningCombinations = [
+  ['1,1', '2,1', '3,1'],
+  ['1,2', '2,2', '3,2'],
+  ['1,3', '2,3', '3,3'],
+  ['1,1', '1,2', '1,3'],
+  ['2,1', '2,2', '2,3'],
+  ['3,1', '3,2', '3,3'],
+  ['1,1', '2,2', '3,3'],
+  ['3,1', '2,2', '1,3'],
+];
+
+startButton.addEventListener('click', startGame);
 
 function startGame() {
-
-    gameContainer.innerHTML = ""
-    player = true
-
-    table.forEach((element, index) => createCells(element, index))
+  gameContainer.innerHTML = '';
+  player = true;
+  playerXSelections = [];
+  playerOSelections = [];
+  createCells();
 }
 
+function createCells() {
+  const table = document.createElement('table');
+  for (let i = 1; i <= 3; i++) {
+    const tr = document.createElement('tr');
 
-function createCells(element, index) {
-    const row = document.createElement("div")
-    row.classList.add("container")
-
-    index === 0 ?
-        row.classList.add(`container${index + 1}`)
-        :
-        index === 1 ?
-            row.classList.add(`container${index + 1}`)
-            :
-            index === 2 ?
-                row.classList.add(`container${index + 1}`)
-                :
-                null
-
-    for (let i = 0; i < element.length; i++) {
-        const cell = element[i]
-        if (cell === "S") {
-            const cell = document.createElement("div")
-            cell.classList.add("cell")
-            cell.dataset.line = index
-            cell.dataset.column = i
-            row.appendChild(cell)
-            cell.addEventListener("click", game)
-        }
+    for (let j = 1; j <= 3; j++) {
+      const td = document.createElement('td');
+      tr.appendChild(td);
+      td.dataset.column = j;
+      td.dataset.line = i;
+      td.addEventListener('click', game);
     }
-    gameContainer.appendChild(row)
+
+    table.appendChild(tr);
+  }
+
+  gameContainer.appendChild(table);
 }
 
 function game(evt) {
-    console.log(evt.target.parentElement)
+  const evtLine = evt.target.dataset.line;
+  const evtColumn = evt.target.dataset.column;
 
-    if (evt.target.className === "cell" && player) {
-        evt.target.classList.add("X")
-        player = !player
-    }
+  if (evt.target.innerHTML == '' && player) {
+    evt.target.innerHTML = 'X';
+    playerXSelections.push(`${evtColumn},${evtLine}`);
+    player = !player;
+  }
 
-    if (evt.target.className === "cell" && !player) {
-        evt.target.classList.add("O")
-        player = !player
-    }
+  if (evt.target.innerHTML == '' && !player) {
+    evt.target.innerHTML = 'O';
+    playerOSelections.push(`${evtColumn},${evtLine}`);
+    player = !player;
+  }
 
+  let win = verifyWin();
 
+  if (!win && playerXSelections.length + playerOSelections.length == 9) {
+    alert('Empate :(');
+    document.querySelectorAll('td').forEach((cell) => {
+      cell.removeEventListener('click', game);
+    });
+  }
 }
 
-function verifyWin(evtTarget) {
+function verifyWin() {
+  let win = false;
 
+  winningCombinations.forEach((combination) => {
+    let matchesX = 0;
+    let matchesO = 0;
+
+    for (let i = 0; i < playerXSelections.length; i++) {
+      let cell = playerXSelections[i];
+      if (combination.includes(cell)) {
+        matchesX++;
+      }
+    }
+
+    for (let i = 0; i < playerOSelections.length; i++) {
+      let cell = playerOSelections[i];
+      if (combination.includes(cell)) {
+        matchesO++;
+      }
+    }
+
+    if (matchesX === 3) {
+      alert('X venceu!');
+      document.querySelectorAll('td').forEach((cell) => {
+        cell.removeEventListener('click', game);
+      });
+      win = true;
+    }
+
+    if (matchesO === 3) {
+      alert('O venceu!');
+      document.querySelectorAll('td').forEach((cell) => {
+        cell.removeEventListener('click', game);
+      });
+      win = true;
+    }
+  });
+
+  return win;
 }
